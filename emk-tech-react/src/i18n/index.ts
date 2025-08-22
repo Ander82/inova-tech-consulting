3b5057032cec0a5ab20e754da699b154792707f1
ANDERSON PEREIRA <59625097+Ander82@users.noreply.github.com>
2025-08-22 11:10:40 -0500

    Add emk-tech-react/src/i18n/index.ts 1755879040660

--- emk-tech-react/src/i18n/index.ts
+++ emk-tech-react/src/i18n/index.ts
@@ -5,17 +5,29 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // Import translations
 import enCommon from '../locales/en/common.json';
 import enHome from '../locales/en/home.json';
+import enTech from '../locales/en/tech.json';
+import enUseCases from '../locales/en/usecases.json';
+import enCta from '../locales/en/cta.json';
 import ptCommon from '../locales/pt-BR/common.json';
 import ptHome from '../locales/pt-BR/home.json';
+import ptTech from '../locales/pt-BR/tech.json';
+import ptUseCases from '../locales/pt-BR/usecases.json';
+import ptCta from '../locales/pt-BR/cta.json';
 
 const resources = {
   en: {
     common: enCommon,
     home: enHome,
+    tech: enTech,
+    usecases: enUseCases,
+    cta: enCta,
   },
   'pt-BR': {
     common: ptCommon,
     home: ptHome,
+    tech: ptTech,
+    usecases: ptUseCases,
+    cta: ptCta,
   },
 };
 
